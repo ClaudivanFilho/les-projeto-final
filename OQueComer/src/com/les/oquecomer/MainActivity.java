a3b62b801d83a01b120dd270484e7f5b8809b69d
claudivanfilho <j.claudivan.filho@gmail.com>
2014-07-21 15:02:39 -0300

    receita aparecendo na tela

--- OQueComer/src/com/les/oquecomer/MainActivity.java
+++ OQueComer/src/com/les/oquecomer/MainActivity.java
@@ -2,22 +2,27 @@
 package com.les.oquecomer;
 
 import java.net.URL;
+import java.util.ArrayList;
 
-import com.les.oquecomer.util.GerenciadorReceita;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import android.app.Activity;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.app.Activity;
-import android.content.Context;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.Toast;
+
+import com.les.oquecomer.util.GerenciadorReceita;
 
 public class MainActivity extends Activity implements OnClickListener {
 
@@ -31,25 +36,32 @@ public class MainActivity extends Activity implements OnClickListener {
 	LinearLayout generalLayout;
 	GerenciadorReceita gerenciador;
 	String[] ingredientes = {"agua","gengibre"};
+	static ListView listField;
+	static ArrayAdapter<String> adapter;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		generalLayout = (LinearLayout) findViewById(R.id.generalLayout);
-		recipeSearch = (ImageButton) findViewById(R.id.recipeSearch);
+		
+		setContentView(R.layout.search);
+//		generalLayout = (LinearLayout) findViewById(R.id.generalLayout);
+//		recipeSearch = (ImageButton) findViewById(R.id.recipeSearch);
 //		recipeBook = (ImageButton) findViewById(R.id.recipeBook);
 //		facebook = (ImageButton) findViewById(R.id.facebook);
 //		site = (ImageButton) findViewById(R.id.site);
-		recipeSearch.setOnClickListener(this);
+		//recipeSearch.setOnClickListener(this);
 //		recipeBook.setOnClickListener(this);
 //		facebook.setOnClickListener(this);
 //		site.setOnClickListener(this);
-		generalLayout.removeAllViews();
+		//generalLayout.removeAllViews();
 //		LinearLayout recipeBookView = (LinearLayout) inflater.inflate(R.layout.recipe_book_view, null);
 //		generalLayout.addView(recipeBookView);
 //		
+		listField = (ListView) findViewById(R.id.listView1);
+		adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, 
+				android.R.id.text1, new ArrayList<String>());
+		listField.setAdapter(adapter);
+		
 		gerenciador = new GerenciadorReceita();
 		
 		new AtualizaListagem().execute();
@@ -65,30 +77,38 @@ public class MainActivity extends Activity implements OnClickListener {
 	@Override
 	public void onClick(View arg0) {
 		if(arg0 == recipeSearch){
-			generalLayout.removeAllViews();
-			LinearLayout search = (LinearLayout) inflater.inflate(R.layout.search, null);
-			generalLayout.addView(search);
-			searchDone = (ImageButton) findViewById(R.id.search_done_button);
-			searchDone.setOnClickListener(this);
-			itemsInput = (EditText) findViewById(R.id.items_text_input);
-			//Toast.makeText(this, "Not Implemented Yet", Toast.LENGTH_SHORT).show();
+			initSearch();
 		}
-		else if(arg0 == recipeBook){
-			generalLayout.removeAllViews();
-			LinearLayout recipeBookView = (LinearLayout) inflater.inflate(R.layout.recipe_book_view, null);
-			generalLayout.addView(recipeBookView);
-			Toast.makeText(this, "Not Implemented Yet", Toast.LENGTH_SHORT).show();
-		}
-		else if(arg0 == facebook){
-			Toast.makeText(this, "Not Implemented Yet", Toast.LENGTH_SHORT).show();
-		}
-		else if(arg0 == site){
-			Toast.makeText(this, "Not Implemented Yet", Toast.LENGTH_SHORT).show();
-		}
-		else if (arg0 == searchDone){
-			Toast.makeText(this, "Shoud be Searching By Now: " + itemsInput.getText(), Toast.LENGTH_SHORT).show();
-		}
+//		else if(arg0 == recipeBook){
+//			generalLayout.removeAllViews();
+//			LinearLayout recipeBookView = (LinearLayout) inflater.inflate(R.layout.recipe_book_view, null);
+//			generalLayout.addView(recipeBookView);
+//			Toast.makeText(this, "Not Implemented Yet", Toast.LENGTH_SHORT).show();
+//		}
+//		else if(arg0 == facebook){
+//			Toast.makeText(this, "Not Implemented Yet", Toast.LENGTH_SHORT).show();
+//		}
+//		else if(arg0 == site){
+//			Toast.makeText(this, "Not Implemented Yet", Toast.LENGTH_SHORT).show();
+//		}
+//		else if (arg0 == searchDone){
+//			Toast.makeText(this, "Shoud be Searching By Now: " + itemsInput.getText(), Toast.LENGTH_SHORT).show();
+//		}
 
+	}
+	
+	public void initSearch() {
+		setContentView(R.layout.search);
+		//searchDone.setOnClickListener(this);
+	}
+	
+	public void searchReceitas(View v) {
+		itemsInput = (EditText) findViewById(R.id.items_text_input);
+		String ingrediente = itemsInput.getText().toString() ;
+		ingredientes = new String[1];
+		ingredientes[0] = ingrediente;
+		
+		new AtualizaListagem().execute();
 	}
 	
 	class AtualizaListagem extends AsyncTask<URL, Integer, Long> {
@@ -109,7 +129,14 @@ public class MainActivity extends Activity implements OnClickListener {
 		 * da activity.
 		 */
 		protected void onPostExecute(Long result) {
-			Log.d("Receitas", gerenciador.getRecceitas().toString());
+			adapter.clear();
+			for(JSONObject j: gerenciador.getRecceitas()) {
+				try {
+					adapter.add(j.getString("nome"));
+				} catch (JSONException e) {
+					Log.d("EROO", e.getMessage());
+				}
+			}
 		}
 	}
 
